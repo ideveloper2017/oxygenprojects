35c795697101cbcb550625b240a87cc93d1ab086
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-09-14 17:44:36 +0500

    Commit

--- src/modules/file-upload/file-upload.controller.ts
+++ src/modules/file-upload/file-upload.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileUploadService } from './file-upload.service';
 import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
@@ -12,62 +19,62 @@ import { RemoveFileDto } from './dto/remove-file.dto';
 @ApiTags('FileUpload')
 @Controller('file-upload')
 export class FileUploadController {
-  constructor(
-    private readonly fileUploadService: FileUploadService) {}
+  constructor(private readonly fileUploadService: FileUploadService) {}
 
   @Post('/upload')
   @ApiConsumes('multipart/form-data')
-
-  @ApiOperation({summary: 'Upload a file', description: 'Upload a file by selecting it using the upload button.',})
+  @ApiOperation({
+    summary: 'Upload a file',
+    description: 'Upload a file by selecting it using the upload button.',
+  })
   @ApiBody({
     schema: {
       type: 'object',
       properties: {
         file: {
           type: 'string',
-          format: 'binary'
-        }
-      }
-    }
+          format: 'binary',
+        },
+      },
+    },
   })
-
   @UseInterceptors(FileInterceptor('file'))
-  
-  async uploadFile(@UploadedFile() file:Express.Multer.File, @Body() fileUploadDto: CreateFileUploadDto) {
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() fileUploadDto: CreateFileUploadDto,
+  ) {
     if (!file) {
-      return {success: false, message:"yuklash uchun rasm tanlanmagan!!"}
+      return { success: false, message: 'yuklash uchun rasm tanlanmagan!!' };
     }
 
     try {
-
-      const uploadingFile = await this.fileUploadService.uploadFile(file, fileUploadDto).then(data => {
-
-        if (data.affected > 0) {
-          return {success: true, message: "file uploaded successfully" }
-        }else {
-          return {success: false, message: "error while uploading" }
-        }
-      })
-      
+      const uploadingFile = await this.fileUploadService
+        .uploadFile(file, fileUploadDto)
+        .then((data) => {
+          if (data.affected > 0) {
+            return { success: true, message: 'file uploaded successfully' };
+          } else {
+            return { success: false, message: 'error while uploading' };
+          }
+        });
     } catch (error) {
       console.log(error);
-      return {success: false, message: 'Fayl yuklashda xatolik'};
+      return { success: false, message: 'Fayl yuklashda xatolik' };
     }
   }
 
   @Post('/remove')
-  @ApiOperation({summary: 'Delete file', description: 'town, bino , kvartira larga boglangan fileni ochirish',})
-
+  @ApiOperation({
+    summary: 'Delete file',
+    description: 'town, bino , kvartira larga boglangan fileni ochirish',
+  })
   removeFile(@Body() removeFileDto: RemoveFileDto) {
-      return this.fileUploadService.deleteFile(removeFileDto).then(data => {
-        if(data === undefined) {
-          return {success: true, message: "File deleted successfully"}
-        }else {
-          return {success: false, message: "File not exists"}
-          
+    return this.fileUploadService.deleteFile(removeFileDto).then((data) => {
+      if (data === undefined) {
+        return { success: true, message: 'File deleted successfully' };
+      } else {
+        return { success: false, message: 'File not exists' };
       }
-    }
-      )
-
+    });
   }
 }
