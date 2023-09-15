186941cf3c5b861e7f655fe40c27dc24ef614f3e
Akram aka <bahrom1982@gmail.com>
2023-09-15 20:48:23 +0500

    Commit Orders change all data

--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -84,7 +84,9 @@ export class OrdersService {
     const oneMonthDue =
       (total - createOrderDto.initial_pay) / createOrderDto.installment_month;
 
-    const paymethod=await this.ordersRepository.manager.getRepository(PaymentMethods).findOne({ where: { id: createOrderDto.payment_method_id } })
+    const paymethod=await this.ordersRepository.manager.getRepository(PaymentMethods)
+        .findOne({ where: { id: createOrderDto.payment_method_id } })
+
     if (paymethod.name.toLowerCase() === 'rassrochka') {
       const creditSchedule = [];
       const date = new Date();
