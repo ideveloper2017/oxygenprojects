c1d515a3a42ccadd7750496950a01c315d760fce
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-09-18 23:25:59 +0500

    Commit

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,17 +7,19 @@ import {
   Param,
   Delete,
   UseGuards,
-  UseInterceptors, ForbiddenException, Req,
+  UseInterceptors,
+  ForbiddenException,
+  Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthLoginDto } from './dto/AuthLoginDto';
 import { AuthUser } from '../../common/decorators/auth-user.decorator';
 import { AuthGuard } from '@nestjs/passport';
-import {CookieInterceptor} from "./interceptor/cookie.interceptor";
-import {LoginResponse} from "./type/loginResponse";
-import {Users} from "../users/entities/user.entity";
-import {UsersService} from "../users/users.service";
+import { CookieInterceptor } from './interceptor/cookie.interceptor';
+import { LoginResponse } from './type/loginResponse';
+import { Users } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcryptjs';
 
 @UseInterceptors(CookieInterceptor)
@@ -25,8 +27,8 @@ import * as bcrypt from 'bcryptjs';
 @Controller('auth')
 export class AuthController {
   constructor(
-      private readonly authService: AuthService,
-      private readonly userService: UsersService,
+    private readonly authService: AuthService,
+    private readonly userService: UsersService,
   ) {}
 
   // @UseGuards(AuthGuard('local'))
@@ -36,15 +38,15 @@ export class AuthController {
   // }
 
   @Post('login')
-  async loginUser(@Body() loginUserDto: AuthLoginDto){
+  async loginUser(@Body() loginUserDto: AuthLoginDto) {
     const { usernameoremail, password: loginPassword } = loginUserDto;
     let existingUser: Omit<Users, 'created_at' | 'updated_at'>;
     let isValid: boolean;
 
     try {
-      existingUser = await this.userService.findUserWithPassword(usernameoremail);
+      existingUser =
+        await this.userService.findUserWithPassword(usernameoremail);
       isValid = await bcrypt.compare(loginPassword, existingUser.password);
-
     } catch (error) {
       throw new ForbiddenException('Username or password is invalid');
     }
@@ -66,11 +68,8 @@ export class AuthController {
     const token = req.cookies['refreshToken'];
 
     try {
-      const {
-        accessToken,
-        refreshToken,
-        user,
-      } = await this.authService.refreshTokens(token);
+      const { accessToken, refreshToken, user } =
+        await this.authService.refreshTokens(token);
       if (accessToken && user) {
         return { accessToken, refreshToken };
       }
@@ -78,7 +77,4 @@ export class AuthController {
       throw new ForbiddenException(error.message);
     }
   }
-
-
-
 }
