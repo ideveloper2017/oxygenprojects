6d8b45e8fbe359d75a4528882482326fb64d00f3
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-09-18 23:49:34 +0500

    Commit

--- src/modules/payments/payments.controller.ts
+++ src/modules/payments/payments.controller.ts
@@ -6,25 +6,79 @@ import {
   Patch,
   Param,
   Delete,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { NewPaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
 @ApiTags('Payments')
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  @ApiOperation({summary: "To'lov amalga oshirish"})
+  @Get('/list')
+  public getAll() {
+    return this.paymentsService
+      .getAll()
+      .then((data) => {
+        if (data.length > 0) {
+          data.map((data) => {
+            return { success: true, data: data, message: 'success' };
+          });
+        } else {
+          return { success: false, message: 'not found record!!!' };
+        }
+      })
+      .catch((error) => {
+        return { success: false, message: error.message };
+      });
+  }
+
+  @Patch('/update/:id')
+  public updatePayment(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() newPaymentDto: UpdatePaymentDto,
+  ) {
+    return this.paymentsService
+      .update(id, newPaymentDto)
+      .then((data) => {
+        if (data.affected > 0) {
+          return { success: true, message: 'update' };
+        } else {
+          return { success: false, message: 'not update' };
+        }
+      })
+      .catch((error) => {
+        return { success: false, message: error.message };
+      });
+  }
+
+  @Delete('/delete/:id')
+  deletePayment(@Param('id', ParseIntPipe) id: number) {
+    return this.paymentsService
+      .delete(id)
+      .then((data) => {
+        if (data.affected > 0) {
+          return { success: true, message: 'deteled' };
+        } else {
+          return { success: false, message: 'not deleted' };
+        }
+      })
+      .catch((error) => {
+        return { success: false, message: error.message };
+      });
+  }
+  @ApiOperation({ summary: "To'lov amalga oshirish" })
   @Post('/new-payment')
-  newPayment(@Body() newPaymentDto: NewPaymentDto){
-    return this.paymentsService.newPayment(newPaymentDto).then(data => {
-      if(!data){
-        return {success: false , message: "Payment was not created"}
-      }else {
-        return {success: true , message: "Payment created"}
+  newPayment(@Body() newPaymentDto: NewPaymentDto) {
+    return this.paymentsService.newPayment(newPaymentDto).then((data) => {
+      if (!data) {
+        return { success: false, message: 'Payment was not created' };
+      } else {
+        return { success: true, message: 'Payment created' };
       }
-    })
+    });
   }
 }
--- src/modules/payments/payments.service.ts
+++ src/modules/payments/payments.service.ts
@@ -1,107 +1,117 @@
 import { Injectable } from '@nestjs/common';
 import { NewPaymentDto } from './dto/create-payment.dto';
-import { UpdatePaymentDto } from './dto/update-payment.dto';
 import { Repository } from 'typeorm';
 import { Payments } from './entities/payment.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Orders } from '../orders/entities/order.entity';
 import { CreditTable } from '../credit-table/entities/credit-table.entity';
+import { UpdatePaymentDto } from './dto/update-payment.dto';
 
 @Injectable()
 export class PaymentsService {
-  constructor (@InjectRepository(Payments) private readonly paymentRepo: Repository<Payments>){}
+  constructor(
+    @InjectRepository(Payments)
+    private readonly paymentRepo: Repository<Payments>,
+  ) {}
 
-  async newPayment(newPaymentDto: NewPaymentDto){
+  async newPayment(newPaymentDto: NewPaymentDto) {
+    const { paymentMethods } = await Orders.findOne({
+      where: { id: newPaymentDto.order_id },
+      relations: ['paymentMethods'],
+    });
+    let newPay;
 
-    const {paymentMethods}= await Orders.findOne({where: {id: newPaymentDto.order_id}, relations: ['paymentMethods']})
-    let newPay
-   
+    // while(check) {
 
-      // while(check) {
+    //   console.log('inside of if:');
 
-      //   console.log('inside of if:');
+    //     let lastPaid = await CreditTable.findOne({where: {status: 'waiting'}, order: {'due_date':'ASC' }})
+    //     // console.log(lastPaid.due_amount-lastPaid.left_amount);
+    //     const leftAmount = money - (lastPaid.due_amount - lastPaid.left_amount)
 
-      //     let lastPaid = await CreditTable.findOne({where: {status: 'waiting'}, order: {'due_date':'ASC' }})
-      //     // console.log(lastPaid.due_amount-lastPaid.left_amount);
-      //     const leftAmount = money - (lastPaid.due_amount - lastPaid.left_amount)
-          
-      //     // break
-      //     if(leftAmount > 0 ){
-      //       console.log(leftAmount);
-            
-      //       if(leftAmount >= lastPaid.due_amount){
-             
-      //         await CreditTable.update({id: lastPaid.id}, {status: 'paid', left_amount: 0})
-      //         money -= lastPaid.due_amount
-      //         check = 1
-            
-      //       }else {
+    //     // break
+    //     if(leftAmount > 0 ){
+    //       console.log(leftAmount);
 
-      //         await CreditTable.update({id: lastPaid.id}, {left_amount: lastPaid.due_amount - leftAmount })
-      //         money-= leftAmount
-      //         check =1
-              
-      //       }
-      //     }else {
-      //       await CreditTable.update({id: lastPaid.id}, {left_amount:lastPaid.due_amount - leftAmount})
-      //       // money-= lastPaid.left_amount
-      //       break
-      //     }
-      //   }
-        // console.log(leftAmount);
-          if(paymentMethods.name.toLowerCase() == 'rassrochka') {
+    //       if(leftAmount >= lastPaid.due_amount){
 
-            let check = 1
-            let money = newPaymentDto.amount     
-          while (money > 0) {
-            const nextPaid = await CreditTable.findOne({
-              where: { status: 'waiting' },
-              order: {'due_date':'ASC'},
-            });
-        
-            if (!nextPaid) {
-              break; // No more unpaid installments, exit the loop
-            }
-        
-            if (money >= nextPaid.due_amount) {
-              await CreditTable.update(
-                { id: nextPaid.id },
-                { status: 'paid', left_amount: 0 }
-              );
-              money -= nextPaid.due_amount;
-            } else {
-              await CreditTable.update(
-                { id: nextPaid.id },
-                { left_amount: +(nextPaid.due_amount - money).toFixed() }
-              );
-              money = 0;
-            }
-          }
-          const payment = new Payments()
-      payment.order_id = newPaymentDto.order_id
-      payment.amount = newPaymentDto.amount
-      payment.payment_date = new Date()
-      payment.by_card= newPaymentDto.by_card
-      payment.in_cash = newPaymentDto.in_cash
-      payment.bank = newPaymentDto.bank
-      
-      newPay = await this.paymentRepo.save(payment)
+    //         await CreditTable.update({id: lastPaid.id}, {status: 'paid', left_amount: 0})
+    //         money -= lastPaid.due_amount
+    //         check = 1
 
-    }else {
-      
-      
-          const payment = new Payments()
-      payment.order_id = newPaymentDto.order_id
-      payment.amount = newPaymentDto.amount
-      payment.payment_date = new Date()
-      payment.by_card= newPaymentDto.by_card
-      payment.in_cash = newPaymentDto.in_cash
-      payment.bank = newPaymentDto.bank
-      
-      newPay = await this.paymentRepo.save(payment)
-      
+    //       }else {
+
+    //         await CreditTable.update({id: lastPaid.id}, {left_amount: lastPaid.due_amount - leftAmount })
+    //         money-= leftAmount
+    //         check =1
+
+    //       }
+    //     }else {
+    //       await CreditTable.update({id: lastPaid.id}, {left_amount:lastPaid.due_amount - leftAmount})
+    //       // money-= lastPaid.left_amount
+    //       break
+    //     }
+    //   }
+    // console.log(leftAmount);
+    if (paymentMethods.name.toLowerCase() == 'rassrochka') {
+      const check = 1;
+      let money = newPaymentDto.amount;
+      while (money > 0) {
+        const nextPaid = await CreditTable.findOne({
+          where: { status: 'waiting' },
+          order: { due_date: 'ASC' },
+        });
+
+        if (!nextPaid) {
+          break; // No more unpaid installments, exit the loop
+        }
+
+        if (money >= nextPaid.due_amount) {
+          await CreditTable.update(
+            { id: nextPaid.id },
+            { status: 'paid', left_amount: 0 },
+          );
+          money -= nextPaid.due_amount;
+        } else {
+          await CreditTable.update(
+            { id: nextPaid.id },
+            { left_amount: +(nextPaid.due_amount - money).toFixed() },
+          );
+          money = 0;
+        }
+      }
+      const payment = new Payments();
+      payment.order_id = newPaymentDto.order_id;
+      payment.amount = newPaymentDto.amount;
+      payment.payment_date = new Date();
+      payment.by_card = newPaymentDto.by_card;
+      payment.in_cash = newPaymentDto.in_cash;
+      payment.bank = newPaymentDto.bank;
+
+      newPay = await this.paymentRepo.save(payment);
+    } else {
+      const payment = new Payments();
+      payment.order_id = newPaymentDto.order_id;
+      payment.amount = newPaymentDto.amount;
+      payment.payment_date = new Date();
+      payment.by_card = newPaymentDto.by_card;
+      payment.in_cash = newPaymentDto.in_cash;
+      payment.bank = newPaymentDto.bank;
+
+      newPay = await this.paymentRepo.save(payment);
     }
-    return newPay
+    return newPay;
   }
-  
+
+  async getAll() {
+    return await this.paymentRepo.find({ relations: ['orders'] });
+  }
+
+  async update(id: number, newPaymentDto: UpdatePaymentDto) {
+    return await this.paymentRepo.update({ id: id }, newPaymentDto);
+  }
+
+  async delete(id: number) {
+    return this.paymentRepo.delete({ id: id });
+  }
 }
