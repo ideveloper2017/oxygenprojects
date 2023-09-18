88291852596951e2d80c68e2f3d6a32086e49497
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-09-18 23:37:21 +0500

    Commit

--- src/modules/caisher/caisher.module.ts
+++ src/modules/caisher/caisher.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CaisherService } from './caisher.service';
 import { CaisherController } from './caisher.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Caisher } from './entities/caisher.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Caisher])],
   controllers: [CaisherController],
   providers: [CaisherService],
 })
