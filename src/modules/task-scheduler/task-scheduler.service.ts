2828894548e5985c509f103257aacda4d39caf7e
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-09-18 12:54:42 +0500

    Commit

--- src/modules/task-scheduler/task-scheduler.service.ts
+++ src/modules/task-scheduler/task-scheduler.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Apartments } from '../apartments/entities/apartment.entity';
 import { Repository } from 'typeorm';
-import {Cron} from '@nestjs/schedule'
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class TaskSchedulerService {
@@ -17,23 +17,24 @@ export class TaskSchedulerService {
   async checkAndChangeApartmentStatus(): Promise<void> {
     this.logger.log('Running apartment status check...');
 
-    const apartments = await this.apartmentRepository.find({where: {status: 'bron'}});
+    const apartments = await this.apartmentRepository.find({
+      where: { status: 'bron' },
+    });
 
     const currentDate = new Date();
-    const threeDaysAgo = new Date(currentDate.getTime() - 3 * 24 * 60 * 60 * 1000);
+    const threeDaysAgo = new Date(
+      currentDate.getTime() - 3 * 24 * 60 * 60 * 1000,
+    );
 
     for (const apartment of apartments) {
       if (apartment.updated_at && apartment.updated_at < threeDaysAgo) {
-
         apartment.status = 'free';
         await this.apartmentRepository.save(apartment);
-        
+
         this.logger.log(`Apartment ${apartment.id} status changed to free.`);
       }
     }
 
     this.logger.log('Apartment status check completed.');
   }
-
 }
-
