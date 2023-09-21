e65f24e085468b26465d4120ccad5edac1dd06c3
Akram aka <bahrom1982@gmail.com>
2023-09-21 06:31:14 +0500

    Client change

--- src/modules/wordexport/wordexport.controller.ts
+++ src/modules/wordexport/wordexport.controller.ts
@@ -21,28 +21,5 @@ export class WordexportController {
     fs.writeFileSync('myTemplate - output.docx', doc);
   }
 
-   saveFile(filename, blob) {
-
-    // see: https://stackoverflow.com/questions/19327749/javascript-blob-filename-without-link
-
-    // get downloadable url from the blob
-    const blobUrl = URL.createObjectURL(blob);
-
-    // create temp link element
-    let link = document.createElement("a");
-    link.download = filename;
-    link.href = blobUrl;
-
-    // use the link to invoke a download
-    document.body.appendChild(link);
-    link.click();
-
-    // remove the link
-    setTimeout(() => {
-      link.remove();
-      window.URL.revokeObjectURL(blobUrl);
-      link = null;
-    }, 0);
-  }
 
 }
