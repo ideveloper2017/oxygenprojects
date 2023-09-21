d7e6daef8a15b7a7cc2f95d5f435f8746f2831f3
Akram aka <bahrom1982@gmail.com>
2023-09-21 08:27:41 +0500

    Client change

--- src/modules/wordexport/wordexport.controller.ts
+++ src/modules/wordexport/wordexport.controller.ts
@@ -16,10 +16,13 @@ export class WordexportController {
   }
   @Get('export/:client_id')
   async exportWord(@Param('client_id') client_id:number,@Res() res) {
+    let client;
     const filename = 'data/contract.docx';
     const templateFile = fs.readFileSync('data/contract.docx');
-   const client= await this.orderRepo.manager.getRepository(Clients).findOne({where:{id:client_id}});
-    const order=await this.orderRepo.findOne({where:{clients:client as FindOptionsWhere<Clients>},relations:['clients']});
+
+
+    client=  await this.orderRepo.manager.getRepository(Clients).findOne({where:{id:client_id}}).then(data=>{return data});
+    const order=await this.orderRepo.findOne({where:{clients:client },relations:['clients']});
     const data = {
       orders: [
         { order_number: order?.id,client_name:order?.clients?.first_name},
