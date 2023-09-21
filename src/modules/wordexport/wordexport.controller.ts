bc4637e50c879fbe671865cd4cc22ab10f0583c2
Akram aka <bahrom1982@gmail.com>
2023-09-21 15:47:11 +0500

    Client change

--- src/modules/wordexport/wordexport.controller.ts
+++ src/modules/wordexport/wordexport.controller.ts
@@ -36,13 +36,7 @@ export class WordexportController {
     });
 
     const data = {
-      orders: [
-        {
-
-          apartment,
-
-        }
-      ]
+         apartment
     };
     console.log(JSON.stringify(data))
 
