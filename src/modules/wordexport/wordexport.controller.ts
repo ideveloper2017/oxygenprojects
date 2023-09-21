3058d5fb5818596966ed58c8ed54b7c334f51233
Akram aka <bahrom1982@gmail.com>
2023-09-21 06:49:28 +0500

    Client change

--- src/modules/wordexport/wordexport.controller.ts
+++ src/modules/wordexport/wordexport.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Res } from '@nestjs/common';
-import fs from "fs";
+// import fs from "fs";
 import {TemplateHandler} from "easy-template-x";
-
+const fs=require('fs');
 @Controller('wordexport')
 export class WordexportController {
   @Get('export')
   async exportWord(@Res() res) {
+    const filename = 'data/contract.docx';
     const templateFile = fs.readFileSync('data/contract.docx');
 
     const data = {
@@ -18,7 +19,12 @@ export class WordexportController {
     const handler = new TemplateHandler();
     const doc = await handler.process(templateFile, data);
 
-    fs.writeFileSync('myTemplate - output.docx', doc);
+    res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
+    res.setHeader('Content-Type', 'application/octet-stream'); // You can set the appropriate MIME type
+    res.send(doc)
+    const fileStream = fs.createReadStream(filename);
+    fileStream.pipe(res);
+    // fs.writeFileSync('myTemplate - output.docx', doc);
   }
 
 
