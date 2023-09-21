f8cdd4f4737c35c4e272ab09235652a2d70299b9
Akram aka <bahrom1982@gmail.com>
2023-09-21 11:31:58 +0500

    Client change

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   app.setGlobalPrefix('/api');
   app.enableCors(  {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://oxy.brainsmart.uz'],
     credentials:true,
     exposedHeaders:['set-cookie']
 
