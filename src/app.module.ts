ac9fd9ef1f5f003e07cb98abca565036cb8bf593
Akram aka <bahrom1982@gmail.com>
2023-09-22 21:42:40 +0500

    Client change

--- src/app.module.ts
+++ src/app.module.ts
@@ -117,7 +117,7 @@ export class AppModule implements NestModule {
 
     configure(consumer: MiddlewareConsumer) {
         consumer
-            .apply(LoggerMiddleware,AuthMiddleware)
+            .apply(AuthMiddleware)
             .forRoutes({path: '*', method: RequestMethod.ALL});
     }
 }
