1ccd9a2969eec261df24b76d53ef2bce053a0ae1
Akram aka <bahrom1982@gmail.com>
2023-10-02 11:31:46 +0500

    Change Apartment, Payment, Order

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -53,7 +53,7 @@ export class ReportService {
         .leftJoin('floor.entrance', 'entrance', 'entrance.id=floor.entrance_id')
         .leftJoin('entrance.buildings', 'buildings', 'buildings.id=entrance.building_id')
         .leftJoin('buildings.towns', 'towns', 'towns.id=buildings.town_id')
-        .select(['towns.name','caishers.caisher_name','payments.payment_date','payments.amount','payments.amount_usd'])
+       // .select(['towns.name','caishers.caisher_name','payments.payment_date','payments.amount','payments.amount_usd'])
         //.where('payments.caisher_type=:cash',{cash:Caishertype.IN})
         .groupBy('payments.id')
         .addGroupBy('towns.id')
