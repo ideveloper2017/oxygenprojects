bb2a32849e5d992c26e2dbab4472ac9d5329114c
Akram aka <bahrom1982@gmail.com>
2023-10-02 12:27:56 +0500

    Change Apartment, Payment, Order

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -64,7 +64,7 @@ export class ReportService {
         .groupBy('payments.payment_date')
         //.addGroupBy('towns.id')
         .addGroupBy('caishers.id')
-        .addGroupBy("payments.paymentmethods")
+      //  .addGroupBy("payments.paymentmethods")
      //   .addGroupBy('payments.payment_date')
         .orderBy('payments.payment_date',"DESC")
         .getRawMany()
