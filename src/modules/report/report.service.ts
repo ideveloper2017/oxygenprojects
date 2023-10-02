dfa2e501aa86efdad1b60477bc33530c62a0ae4f
Akram aka <bahrom1982@gmail.com>
2023-10-02 16:37:18 +0500

    Change Apartment, Payment, Order

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -117,9 +117,9 @@ export class ReportService {
         .addSelect('SUM(payments.amount_usd)','total_usd')
 
         .where('payments.caisher_type= :cash)',{cash:Caishertype.OUT})
-        .andWhere('town.id= :town_id',{town_id:town_id})
-        .andWhere('caishers.id= :caisher_id',{caisher_id:caisher_id})
-        .andWhere('payments.paymentmethods= :paymentmethods', {paymentmethods:paymentmethods})
+        // .andWhere('town.id= :town_id',{town_id:town_id})
+        // .andWhere('caishers.id= :caisher_id',{caisher_id:caisher_id})
+        // .andWhere('payments.paymentmethods= :paymentmethods', {paymentmethods:paymentmethods})
         .groupBy('payments.paymentmethods')
         .addGroupBy('towns.id')
         .addGroupBy('caishers.id')
