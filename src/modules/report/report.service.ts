8b35366cefb9d0e7c4a7d401c801af32a2803da3
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-10-04 12:24:36 +0500

    Order Active Change

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -14,6 +14,7 @@ import { Payments } from '../payments/entities/payment.entity';
 import { Caishertype } from '../../common/enums/caishertype';
 import { OrderItems } from '../order-items/entities/order-item.entity';
 import * as moment from 'moment/moment';
+import {groupBy} from "rxjs";
 
 @Injectable()
 export class ReportService {
@@ -67,6 +68,7 @@ export class ReportService {
         'orders.order_date>= :startDate and orders.order_date<= :endDate',
         { startDate: startDate, endDate: endDate },
       )
+       .groupBy('towns_name')
       .getRawMany();
 
     resultRes = await Promise.all(
