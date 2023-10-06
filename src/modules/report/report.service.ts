5f5d3b4af24cc01eb8806045c4c1f9920c69e80a
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-10-06 12:38:12 +0500

    Order Active Change

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -528,8 +528,13 @@ export class ReportService {
         });
         data['total_sum_out'] = Number(summa_out.total_sum_out);
         data['total_sum_out_usd'] = Number(summa_out.total_usd_out);
-        data['due_total_sum'] = Number(data.total_amount - summa_out.total_sum_out);
-        data['due_total_usd'] = Number(data.total_amount_usd - summa_out.total_usd_out);
+        data['due_total_sum'] = Number(
+          data.total_amount - summa_out.total_sum_out,
+        );
+        data['due_total_usd'] = Number(
+          data.total_amount_usd - summa_out.total_usd_out,
+        );
+        console.log(data);
         return data;
       }),
     );
