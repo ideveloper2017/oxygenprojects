1da9be44c69367de28dbd31fd80426916cd73c90
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-10-21 01:19:51 +0500

    Commit

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -122,25 +122,37 @@ export class UsersService {
   // }
 
   public async updateUser(id: number, updateUserDto: UpdateUserDto) {
+    console.log(updateUserDto);
+    let obj;
 
-    const user=new Users();
-        user.first_name= updateUserDto.first_name;
-        user.last_name=updateUserDto.last_name;
-        // user.username: updateUserDto.username,
-        user.phone_number= updateUserDto.phone_number;
-        // if (updateUserDto.password){
-          user.password= await bcrypt.hash(updateUserDto.password, 10);
-
-        // }
-        user.is_active= updateUserDto.is_active;
-        user.roles= await Roles.findOne({ where: { id: updateUserDto.role_id } });
-        user.town_access=updateUserDto.town_access?.join(',');
-        user.user_is_deleted=false;
+    if (updateUserDto.password!=null) {
+      obj = {
+        first_name: updateUserDto.first_name,
+        last_name: updateUserDto.last_name,
+        // username: updateUserDto.username,
+        phone_number: updateUserDto.phone_number,
+        password: await bcrypt.hash(updateUserDto.password, 10),
+        is_active: updateUserDto.is_active,
+        roles: await Roles.findOne({where: {id: updateUserDto.role_id}}),
+        town_access: updateUserDto.town_access?.join(','),
+        user_is_deleted: false,
+      }
+    } else {
+      obj = {
+        first_name: updateUserDto.first_name,
+        last_name: updateUserDto.last_name,
+        phone_number: updateUserDto.phone_number,
+        is_active: updateUserDto.is_active,
+        roles: await Roles.findOne({where: {id: updateUserDto.role_id}}),
+        town_access: updateUserDto.town_access?.join(','),
+        user_is_deleted: false,
+      }
+    }
 
 
     return await this.usersRepository.update(
       { id: id },
-        user,
+      obj
     );
   }
 
