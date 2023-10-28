b510adf6e2ff5bc4f5b3d11436011b7bcce3c20c
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-10-29 03:06:58 +0500

    Commit

--- src/modules/wordexport/wordexport.controller.ts
+++ src/modules/wordexport/wordexport.controller.ts
@@ -147,7 +147,7 @@ export class WordexportController {
       } else if (num < 100) {
         return tens[Math.floor(num / 10)] + " " + ones[num % 10];
       } else {
-        return ones[Math.floor(num / 100)] + " hundred " + convertLessThanThousand(num % 100);
+        return ones[Math.floor(num / 100)] + " yuz " + convertLessThanThousand(num % 100);
       }
     }
 
