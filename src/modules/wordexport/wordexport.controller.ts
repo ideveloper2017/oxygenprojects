89ba3932f3138cad8938f41304740862dfe98e8f
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-10-29 03:43:39 +0500

    Commit

--- src/modules/wordexport/wordexport.controller.ts
+++ src/modules/wordexport/wordexport.controller.ts
@@ -148,7 +148,7 @@ export class WordexportController {
       } else if (num < 100) {
         return tens[Math.floor(num / 10)] + " " + ones[num % 10];
       } else {
-        return ones[Math.round(num / 100)] + " юз " + convertLessThanThousand(num % 100);
+        return ones[(+num / 100)] + " юз " + convertLessThanThousand(num % 100);
       }
     }
 
