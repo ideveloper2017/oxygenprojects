04d40e8a7532e9434175ba14e7905bec37eaaed3
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-10-29 14:36:10 +0500

    Commit

--- src/modules/wordexport/wordexport.module.ts
+++ src/modules/wordexport/wordexport.module.ts
@@ -3,10 +3,11 @@ import { WordexportController } from './wordexport.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Orders} from "../orders/entities/order.entity";
 import {CurrenciesModule} from "../currencies/currencies.module";
+import {FileUploadModule} from "../file-upload/file-upload.module";
 
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Orders]), forwardRef(() =>CurrenciesModule)],
+  imports:[TypeOrmModule.forFeature([Orders]), forwardRef(() =>CurrenciesModule),FileUploadModule],
   controllers: [WordexportController],
 
 
