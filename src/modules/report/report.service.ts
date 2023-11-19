e31a0dc53344e7a91c0b2333b093caffdbb79127
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-11-20 01:35:37 +0500

    create return report

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -794,7 +794,7 @@ export class ReportService {
         'towns',
         'towns.id=buildings.town_id',
       )
-      .where('buildings.id= :building_id', { building: building_id })
+      .where('buildings.id= :building_id', { building_id: building_id })
       .getRawMany();
   }
 
