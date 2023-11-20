c28ccb955d14223736d268ef0e2df85d45b1a12e
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-11-21 01:12:39 +0500

    create return report

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -935,6 +935,7 @@ export class ReportService {
     let result;
     result = this.orderRepo.manager
       .createQueryBuilder(CreditTable, 'credittable')
+      .select(['credittable.due_date', 'credittable.due_amount'])
       .where('credittable.order_id= :order_id', { order_id: order_id })
       .getRawMany();
     return result;
