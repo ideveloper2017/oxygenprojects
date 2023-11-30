95fe40008a1123b35b1aeb386f8841523bdb40d9
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-11-30 23:35:55 +0500

    create return report

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -485,7 +485,7 @@ export class ReportService {
           return response;
         });
         data['total_sum_out'] = Math.floor(Number(summa_out.total_sum_out));
-        data['total_sum_out_usd'] = Math.floor(Number(summa_out.total_usd_out));
+        data['total_sum_out_usd'] = summa_out.total_usd_out;
         data['due_total_sum'] =Math.floor(Number(data.total_amount)) - Math.floor(Number(summa_out.total_sum_out));
         data['due_total_usd'] =Math.floor(Number(data.total_amount_usd)) - Math.floor(Number(summa_out.total_usd_out));
         return data;
