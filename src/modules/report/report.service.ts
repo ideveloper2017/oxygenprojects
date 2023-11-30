69735f596719154fec862582d0521be8d1edd122
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-11-30 23:31:49 +0500

    create return report

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -487,7 +487,7 @@ export class ReportService {
         data['total_sum_out'] = Number(summa_out.total_sum_out);
         data['total_sum_out_usd'] = Number(summa_out.total_usd_out);
         (data['due_total_sum'] =
-         Math.floor(Number(data.total_amount) - Number(summa_out.total_sum_out))),
+         Math.floor(Number(data.total_amount)) - Math.floor(Number(summa_out.total_sum_out))),
           (data['due_total_usd'] =
             Number(data.total_amount_usd) - Number(summa_out.total_usd_out)),
           console.log(data);
