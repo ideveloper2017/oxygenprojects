37eee3885bfca8df597159fe599225f73003ccbb
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-12-05 01:14:16 +0500

    create return report

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -566,6 +566,7 @@ export class ReportService {
         'buildings.id as build_id',
         'towns.name as townname',
         'buildings.name as buildingname',
+        'SUM(apartments.room_space) as room_space',
         'SUM(orders.total_amount) as total_amount',
       ])
       .where('orders.order_status IN(:...orderStatus)', {
