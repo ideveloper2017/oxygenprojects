00fbe57c63bf72793c4003ab6d68ff232c6a7137
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-12-07 21:09:23 +0500

    create return report

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -859,7 +859,7 @@ export class ReportService {
       .andWhere('payments.caisher_type=:caisher_type', {
         caisher_type: Caishertype.OUT,
       })
-      .andWhere('orders.is_deleted= :isDelete', { isDelete: true })
+    //  .andWhere('orders.is_deleted= :isDelete', { isDelete: true })
       .groupBy('towns.id')
       .addGroupBy('buildings.id')
       .getRawMany();
