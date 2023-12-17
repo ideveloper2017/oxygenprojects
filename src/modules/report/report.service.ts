615360cd5f5ca46d7a13e497bac3f97e423396ac
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-12-17 16:02:06 +0500

    create return report

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -433,7 +433,7 @@ export class ReportService {
         'buildings.apartment_number as apartment_number',
         'towns.name as townname',
         'buildings.name as buildingname',
-        'buildings.mk_price',
+        'buildings.mk_price as mk_price',
       ])
       .orderBy('building_id', 'ASC')
       .getRawMany();
@@ -825,11 +825,9 @@ export class ReportService {
           const real_price = Number(data.price) - Number(buildin_price);
           summa_real += Number(real_price) * Number(data.room_space);
         });
-
         const order_apartment = await this.orderAllApartment(data.build_id);
         data['all_room_space'] = all_room_space;
-        data['total_room_price'] =
-            (Number(all_room_space) * Number(data.mk_price)) + summa_real;
+        data['total_room_price'] = (Number(all_room_space) * Number(data.mk_price)) + summa_real;
         data['order_room_space'] = order_apartment?.room_space;
         data['order_all_price'] = order_apartment?.total_amount;
         data['total_sum_cash'] = Number(summa.total_sum);
