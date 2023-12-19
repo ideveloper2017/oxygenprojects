bf7dd4fff7f093ebdd0733b2428a173cec664cb8
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-12-19 16:01:47 +0500

    create return report

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -957,7 +957,7 @@ export class ReportService {
           const order_apartment = await this.orderAllApartment(data.build_id);
 
           data['all_room_space'] = all_room_space;
-          data['total_room_price'] =order_apartments_out;
+          data['total_room_price'] =summa_real_out;
            // Number(all_room_space) * Number(data.mk_price) + (summa_real-summa_real_out);
           data['order_room_space'] = order_apartment?.room_space;
           data['order_all_price'] = order_apartment?.total_amount;
