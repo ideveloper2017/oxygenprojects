8de339e7f67728e2df5d5d7271b55dc310710752
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-12-21 17:08:52 +0500

    Order Active Change

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -974,15 +974,17 @@ export class ReportService {
 
           let summa_real = 0;
           order_apartments.forEach((orderData) => {
+
             const real_price =Number(orderData.mk_price)- Number(orderData.price);
             summa_real += Number(real_price) * Number(orderData.room_space);
+
             console.log(Number(orderData.price) + '-' + Number(orderData.mk_price)+' '+summa_real);
           });
 
           // const order_apartment = await this.orderAllApartment(data.build_id);
           data['all_room_space'] = all_room_space.all_room_space;
 
-          data['total_room_price'] =(Number(all_room_space.all_room_price)) +summa_real;
+          data['total_room_price'] =(Number(all_room_space.all_room_price)) -summa_real;
           // data['order_room_space'] = order_apartment?.room_space;
           // data['order_all_price'] = order_apartment?.total_amount;
           data['total_sum_cash'] =
@@ -990,7 +992,7 @@ export class ReportService {
           data['total_sum_bank'] =
             Number(summabank.total_sum) - Number(summabank_out.total_sum);
           data['total_sum_due'] =
-            (Number(all_room_space.all_room_price)+summa_real) -
+            (Number(all_room_space.all_room_price)-summa_real) -
             (Number(summabank.total_sum) -
               Number(summabank_out.total_sum) +
               (Number(summa.total_sum) - Number(summa_out.total_sum)));
